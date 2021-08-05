7d2e69ff70e225d5ba48b7b20850a27c82f6972b
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-05 17:17:59 -0500

    se creo el boton purple de descriptionplace y se acomodo de una para mostrarte en la app

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
   String namePlace;
@@ -69,11 +70,7 @@ class DescriptionPlace extends StatelessWidget {
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        title_stars,
-        description,
-        //ButtonPurple("Navigate")
-      ],
+      children: <Widget>[title_stars, description, ButtonPurple("Navigate")],
     );
   }
 }
