65629cb22b11802b2d8f97553c9dc1d0e41d5f8b
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-05 12:19:08 -0500

    se quito el boton que viene defecto creado con las clases internas y se creo el scaffold para poder enviar informacion a la interfaz, aun no se envia la segunda vista al main

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // poner el banner DEBUG oculto
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false, // poner el banner DEBUG oculto
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: //MyHomePage(title: 'Flutter Demo Home Page'),
+
+            Scaffold(
+          appBar: AppBar(
+            title: Text("Hola Mundo desde scaffold"),
+          ),
+          body: Center(
+            child: Text("Hola mundo desde el hijo del body"),
+          ),
+        ));
   }
 }
 
+
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -112,4 +122,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+}*/
