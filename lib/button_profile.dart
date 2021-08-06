23ab122c5a28fbf00ec480d29bacaf1f39b90aea
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-06 10:01:14 -0500

    se creo la clase button profile pero aun no se inicia a modificar

---
+++ lib/button_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ButtonProfile extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _ButtonProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
