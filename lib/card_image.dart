1d6fa7b7313b6771828963163aea9e00d1b58fca
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-07 08:31:00 -0500

    se soluciono un error de merge olvidado en la clase Card_image, solo se ahbia borrado la linea de iniciacion de la clase

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'floating_action_button_green.dart';
-<<<<<<< HEAD
 
-class  CardImage extends StatelessWidget {
-=======
->>>>>>> 807d730 (se creo el boton flotante de favorito con las propiedades para enviar un mensaje emergente en un showBar y cambiar el icono segun sea la seleccion y se mostro correctamente en la app)
-
+class CardImage extends StatelessWidget {
   String pathImage = "assets/img/beach.jpeg";
 
   CardImage(this.pathImage);
@@ -17,42 +13,23 @@ class  CardImage extends StatelessWidget {
     final card = Container(
       height: 350.0,
       width: 250.0,
-      margin: EdgeInsets.only(
-        top: 80.0,
-        left: 20.0
-
-      ),
-
+      margin: EdgeInsets.only(top: 80.0, left: 20.0),
       decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-            image: AssetImage(pathImage)
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        shape: BoxShape.rectangle,
-        boxShadow: <BoxShadow>[
-          BoxShadow (
-            color:  Colors.black38,
-            blurRadius: 15.0,
-            offset: Offset(0.0, 7.0)
-          )
-        ]
-
-      ),
+          image:
+              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage)),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black38,
+                blurRadius: 15.0,
+                offset: Offset(0.0, 7.0))
+          ]),
     );
 
     return Stack(
-<<<<<<< HEAD
-      alignment: Alignment(0.9,1.1),
-      children: <Widget>[
-        card,
-        FloatingActionButtonGreen()
-      ],
-=======
       alignment: Alignment(0.9, 1.1),
       children: <Widget>[card, FloatingActionButtonGreen()],
->>>>>>> 807d730 (se creo el boton flotante de favorito con las propiedades para enviar un mensaje emergente en un showBar y cambiar el icono segun sea la seleccion y se mostro correctamente en la app)
     );
   }
-
 }
