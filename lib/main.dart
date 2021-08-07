6faeb41c8aa3c73b48ac828c3c58acd41cb4fa19
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-07 08:19:26 -0500

    se borra el debug y se cambia el titulo de la app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // poner el banner DEBUG oculto
+      title: 'Flutter App Curso',
       theme: ThemeData(
           // This is the theme of your application.
           //
