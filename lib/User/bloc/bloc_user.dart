0cdd376fcab3bc3230c293205cf76cb9c10ff8d0
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-07 20:32:57 -0500

    se creo y organizo la logica del login, siendo llamado el bloc desde el screen de login

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -1,8 +1,22 @@
+import 'package:app1/User/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class UserBloc implements Bloc {
+  final _auth_repository = AuthRepository();
+
+  //CASOS DE USO
+  //1. SignIn a la aplicacion Google
+  signIn() {
+    Future<FirebaseUser> signIn() {
+      return _auth_repository.signInFirebase();
+      //este es el metodo que se llama desde la interfaz de usuario
+    }
+  }
+
   @override
   void dispose() {
+    //desde aca llamaremos los repositorios de usuarios para hacer uso de la logica
     // TODO: implement dispose
   }
 }
--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -1,6 +1,9 @@
+import 'package:app1/User/bloc/bloc_user.dart';
 import 'package:app1/widgets/button_green.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '/widgets/gradient_back.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -11,10 +14,12 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreen extends State<SignInScreen> {
+  late UserBloc userBolc;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
+    userBolc = BlocProvider.of(context);
     return signInGoogleUI();
   }
 
@@ -41,7 +46,11 @@ class _SignInScreen extends State<SignInScreen> {
               ),
               ButtonGreen(
                 nameButton: "Login with Gmail",
-                onPressed: () {},
+                onPressed: () {
+                  //con el metodo bloc lo vamos a llamar aca para hacer uso de la logica en el Caso 1.
+                  userBolc.signIn().then((FirebaseUser user) =>
+                      print("El usuario es ${user.displayName}"));
+                },
                 heigthButton: 50.0,
                 widthButton: 300.0,
               ),
